0536481a79652a13b4a1fd8c040a4840e16fe303
FixMaestro <FixMaestro@gmail.com>
2021-12-16 10:23:06 -0600

    initial drop

---
+++ TAP.Shared/Dialogs/NotificationDialog.xaml.cs
@@ -0,0 +1,19 @@
+﻿#if HAS_WINUI
+using Microsoft.UI.Xaml.Controls;
+#else
+using Windows.UI.Xaml.Controls;
+#endif
+
+namespace TAP.Dialogs
+{
+    /// <summary>
+    /// Interaction logic for NotificationDialog.xaml
+    /// </summary>
+    public partial class NotificationDialog : ContentControl
+    {
+        public NotificationDialog()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ TAP.Shared/Views/Shell.xaml.cs
@@ -0,0 +1,22 @@
+using System.Windows;
+
+
+#if HAS_WINUI
+using Microsoft.UI.Xaml.Controls;
+#else
+using Windows.UI.Xaml.Controls;
+#endif
+
+namespace TAP.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class Shell : ContentControl
+    {
+        public Shell()
+        {
+            InitializeComponent();
+        }
+    }
+}
